09de0eb3c5f1f88db64b8a6deac8a58397181cf9
jeric realubit <jericrealubit@gmail.com>
2022-08-15 15:55:16 +1200

    done

--- watchers/src/App.vue
+++ watchers/src/App.vue
@@ -1,12 +1,58 @@
 
-
 <template>
-
+  <h2>Volume Tracker (0-20)</h2>
+  <h3>Current Volume: {{ volume }}</h3>
+  <button @click="volume += 1">Increase</button>
+  <button @click="volume -= 1">Decrease</button>
+  <br>
+  <input type="text" v-model="movie">
+  <input type="text" v-model="movieInfo.title">
+  <input type="text" v-model="movieInfo.actor">
 </template>
 
 <script>
-
-
+  export default {
+    data() {
+      return {
+        volume: 0, 
+        movie: 'spiderman',
+        movieInfo: {
+          title: '',
+          actor: ''
+        }
+      }
+    },
+    computed: {},
+    methods: {
+      blur() {
+        console.log(this.movie)
+      }
+    },
+    watch: {
+      volume(newValue, oldValue) {
+        console.log("new: " + newValue +  " old: " + oldValue)
+        if (newValue === 15 && newValue > oldValue) {
+          alert("Listening to a high volume for a long time may damage your hearing");
+        }
+      },      
+      // movie(newValue) { // wathing on template change
+      //   console.log(newValue)
+      // },      
+      movie: { 
+        handler(newValue) {
+          console.log(newValue)
+        },
+        immediate: true // watching on data property load
+      },
+      movieInfo: {
+        handler(newValue) {
+          console.log(`Calling API with movie title = ${newValue.title} and actor = ${newValue.actor}`)
+        },
+        deep: true
+      }
+    }
+  }
+  
 </script>
 
 
